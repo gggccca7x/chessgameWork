0e4d949332c67b6e9a82e183942c4d6305aea83d
George <george@pulseguard.org>
2017-10-17 14:18:35 +0100

    message to say how to input eveything

--- src/com/pulseguard/pluralsight/UserInterface.java
+++ src/com/pulseguard/pluralsight/UserInterface.java
@@ -7,6 +7,7 @@ public class UserInterface {
     public String playerMove(String player){
         Scanner sc = new Scanner(System.in);
         System.out.println("Turn to move: " + player);
+        System.out.println("NOTE: FOR NOW ALL INPUTS ARE IN FORM 'a2', small letter followed by number");
         System.out.print("Select a piece to move: ");
         String moveFrom = sc.nextLine();
         System.out.print("Select where to move this piece: ");
